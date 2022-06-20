c14f2218d26cb75049551efa37873088a4b409b6
Vyachkoslav <dedplantoed@gmail.com>
2022-06-20 10:02:31 +0300

    Added minimap

---
+++ Assets/BoatGame/Scripts/ObjectFollower.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollower : MonoBehaviour
+{
+    [SerializeField] Transform objectToFollow;
+    Vector3 offset;
+
+    void Start()
+    {
+        if (objectToFollow)
+            offset = transform.position - objectToFollow.position;
+    }
+    void Update()
+    {
+        if(objectToFollow)
+            transform.position = objectToFollow.position + offset;
+    }
+}
